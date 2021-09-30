6b5b80ba7c73233dc9d7320726c43eb0a49ada05
Doron Adler <doron@glidetalk.com>
2021-09-30 19:02:27 +0300

    Revert "Clone while building docker file rather than downloading to container cache"
    This reverts commit c062ea4d6e963c687914c90a0d5cea273f352ff7.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,6 @@ FROM python:3.8.6
 ADD . /app
 WORKDIR /app
 RUN pip3 install -r requirements.txt
-RUN sh ./download.sh
 RUN python3 setup.py
 CMD gunicorn main:app -w 1 -b 0.0.0.0:8080
 
--- download.sh
+++ download.sh
@@ -1,4 +1,7 @@
 
 #!/bin/sh
 
-git clone https://huggingface.co/Norod78/hebrew-bad_wiki-gpt_neo-tiny
+wget https://norod78.s3-eu-west-1.amazonaws.com/models/hewiki_distilGPT2.zip
+mkdir hewiki_distilGPT2
+unzip -n -j hewiki_distilGPT2.zip -d ./hewiki_distilGPT2
+rm hewiki_distilGPT2.zip
